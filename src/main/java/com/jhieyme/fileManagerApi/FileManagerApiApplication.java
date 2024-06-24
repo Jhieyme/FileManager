e4dfc0e85077b973bcf6b71e32ca58f6ac173bcc
jennifer <jennifertorrespalomino@gmail.com>
2024-06-23 20:18:02 -0500

    New update to FileManagerApiApplication

--- src/main/java/com/jhieyme/fileManagerApi/FileManagerApiApplication.java
+++ src/main/java/com/jhieyme/fileManagerApi/FileManagerApiApplication.java
@@ -2,12 +2,31 @@ package com.jhieyme.fileManagerApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FileManagerApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FileManagerApiApplication.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
